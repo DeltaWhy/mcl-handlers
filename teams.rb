7e1d97cbbae4701994857c538a45afc532a5a996
Michael Limiero <mike5713@gmail.com>
2014-12-29 19:22:44 -0800

    Add UHC handler

--- teams.rb
+++ teams.rb
@@ -7,10 +7,12 @@ module DeltaWhy
   # !teams clear
   # !teams spectate
   # !teams join <team> [<player> ...]
+  # !teams leave [<player> ...]
   class Teams < Mcl::Handler
     def setup
       register_teams(:guest, setup: :mod, randomize: :mod, clear: :mod,
-                     spectate: :guest, join: :member, join_others: :mod)
+                     spectate: :guest, join: :member, join_others: :mod,
+                     leave: :member, leave_others: :mod)
       register_parsers
     end
 
@@ -25,7 +27,8 @@ module DeltaWhy
            ["randomize (-t <num_teams> | -p <num_players>)", "randomly assign teams"],
            ["clear", "delete default teams"],
            ["spectate", "become a spectator"],
-           ["join <team> [<player> ...]", "join a team or add players to a team"]]
+           ["join <team> [<player> ...]", "join a team or add players to a team"],
+           ["leave [<player> ...]", "leave or remove players from a team"]]
       d.each do |cmd, desc|
         trawt(player, "Teams", {text: cmd, color: "gold"},
               {text: " #{desc}", color: "reset"})
@@ -82,6 +85,16 @@ module DeltaWhy
               $mcl.server.invoke "/scoreboard teams join #{args[1]} #{p}"
             end
           end
+        when "leave"
+          acl_verify(player, acl_levels[:leave])
+          if args.length == 1
+            $mcl.server.invoke "/scoreboard teams leave #{player}"
+          else
+            acl_verify(player, acl_levels[:leave_others])
+            args[1..args.length].each do |p|
+              $mcl.server.invoke "/scoreboard teams leave #{p}"
+            end
+          end
         else
           usage(player)
         end
@@ -91,9 +104,6 @@ module DeltaWhy
     def register_parsers
       register_parser(/\AFound (.+)\z/) do |res, r|
         Teams.spectators << r[1]
-      end
-      register_parser(/\A(\S+) (fell|was doomed to fall|was struck by lightning|went up in flames|walked into fire whilst fighting|burned to death|was burned to a crisp whilst fighting|tried to swim in lava|suffocated in a wall|drowned|starved to death|was pricked to death|walked into a cactus whilst trying to escape|died|blew up|was blown up by|was killed by magic|withered away|was squashed by a falling|was (slain|shot|fireballed|pummeled|killed) by|was killed trying to hurt|hit the ground too hard)\z/) do |res, r|
-        $mcl.log.info("#{r[1]} died")
       end
     end
 
---
+++ uhc.rb
@@ -0,0 +1,150 @@
+module DeltaWhy
+  Mcl.reloadable(:UHC, parent = DeltaWhy)
+  ## UHC
+  # !uhc help
+  # !uhc option [<key> [<value>]]
+  # !uhc pre
+  # !uhc start
+  # !uhc stop
+  class UHC < Mcl::Handler
+    def setup
+      register_uhc(:mod)
+      register_parsers
+    end
+
+    def usage(player)
+      d = [["option [<key> [<value>]]", "get or set game options"],
+           ["pre", "set up for pre-game"],
+           ["start", "start the game"],
+           ["stop", "stop the game"]]
+      d.each do |cmd, desc|
+        trawt(player, "UHC", {text: cmd, color: "gold"},
+              {text: " #{desc}", color: "reset"})
+      end
+    end
+
+    def register_uhc acl_level
+      register_command :uhc, desc: "UHC game helpers", acl: acl_level do |player, args|
+        case args[0]
+        when "option"
+          if args.length == 1
+            UHC.options.each {|k,v|
+              trawt(player, "UHC", {text: "#{k}: ", color: "aqua"},
+                    {text: v, color: "gold"})
+            }
+          elsif args.length == 2
+            v = UHC.options[args[1].to_sym]
+            if v != nil
+              trawt(player, "UHC", {text: "#{args[1].to_sym}: ", color: "aqua"},
+                    {text: v, color: "gold"})
+            else
+              trawt(player, "UHC", {text: "No such option.", color: "red"})
+            end
+          elsif args.length == 3
+            v = UHC.options[args[1].to_sym]
+            if v == nil
+              trawt(player, "UHC", {text: "No such option.", color: "red"})
+            elsif v.is_a? Integer
+              UHC.options[args[1].to_sym] = args[2].to_i
+            elsif v == false || v == true
+              UHC.options[args[1].to_sym] = (args[2].downcase != "false")
+            elsif v.is_a? String
+              UHC.options[args[1].to_sym] = args[2]
+            end
+          else
+            usage(player)
+          end
+        when "pre"
+          $mcl.server.invoke "/time set day"
+          $mcl.server.invoke "/difficulty peaceful"
+          $mcl.server.invoke "/gamerule doDaylightCycle false"
+          $mcl.server.invoke "/gamerule naturalRegeneration true"
+          $mcl.server.invoke "/setworldspawn 0 255 0"
+          $mcl.server.invoke "/worldborder center 0 0"
+          $mcl.server.invoke "/worldborder set #{2*UHC.options[:border_radius]}" if UHC.options[:border_radius] > 0
+        when "start"
+          $mcl.server.invoke "/effect @a[m=0] minecraft:saturation 1 100"
+          $mcl.server.invoke "/effect @a[m=0] minecraft:instant_health 1 100"
+          $mcl.server.invoke "/effect @a[m=0] minecraft:slowness 9999 100"
+          $mcl.server.invoke "/effect @a[m=0] minecraft:mining_fatigue 9999 100"
+          $mcl.server.invoke "/effect @a[m=0] minecraft:blindness 9999"
+          $mcl.server.invoke "/clear @a[m=0]"
+          #$mcl.server.invoke "/spreadplayers 0 0 #{UHC.options[:min_scatter_distance]} #{UHC.options[:scatter_radius]} true @a[m=0]"
+          traw "@a", "Game starts in #{UHC.options[:start_delay]} seconds!", color: "gold" 
+          schedule "start UHC", Time.now+UHC.options[:start_delay].seconds, StartTask.new
+        when "stop"
+          Task.where(name: "episode marker").destroy_all
+          traw "@a", "Game stopped.", color: "gold"
+        else
+          usage(player)
+        end
+      end
+    end
+
+    def register_parsers
+      register_parser(/\A(\S+) (fell|was doomed to fall|was struck by lightning|went up in flames|walked into fire whilst fighting|burned to death|was burned to a crisp whilst fighting|tried to swim in lava|suffocated in a wall|drowned|starved to death|was pricked to death|walked into a cactus whilst trying to escape|died|blew up|was blown up by|was killed by magic|withered away|was squashed by a falling|was (slain|shot|fireballed|pummeled|killed) by|was killed trying to hurt|hit the ground too hard)\z/) do |res, r|
+        $mcl.log.info("#{r[1]} died")
+        $mcl.server.invoke "/gamemode 3 #{r[1]}"
+        $mcl.log.info("invoked gamemode")
+        if UHC.options[:deathban]
+          traw r[1], "Thanks for playing! You will be kicked in 30 seconds.", color: "gold"
+          schedule "deathban #{r[1]}", Time.now+30.seconds, CommandTask.new("/ban #{r[1]}")
+          $mcl.log.info("scheduled deathban")
+        end
+      end
+    end
+
+    def self.options
+      @options ||= {start_delay: 5,
+                    deathban: false,
+                    episode_duration: -1,
+                    border_radius: 1500,
+                    min_border_radius: -1,
+                    border_duration: -1,
+                    scatter_radius: 1000,
+                    min_scatter_distance: 100,
+                   }
+    end
+
+    class StartTask
+      def initialize
+      end
+
+      def perform!
+        $mcl.server.invoke "/difficulty hard"
+        $mcl.server.invoke "/gamerule doDaylightCycle true"
+        $mcl.server.invoke "/time set day"
+        $mcl.server.invoke "/gamerule naturalRegeneration false"
+        $mcl.server.invoke "/effect @a[m=0] minecraft:slowness 0"
+        $mcl.server.invoke "/effect @a[m=0] minecraft:mining_fatigue 0"
+        $mcl.server.invoke "/effect @a[m=0] minecraft:blindness 0"
+        if UHC.options[:border_duration] > 0 && UHC.options[:min_border_radius] >= 0
+          $mcl.server.invoke "/worldborder set #{2*UHC.options[:min_border_radius]} #{UHC.options[:border_duration]}"
+        end
+        $mcl.server.traw "@a", "Let the games begin!", color: "red"
+        $mcl.scheduler.schedule "episode marker", Time.now+UHC.options[:episode_duration].minutes, EpisodeTask.new(UHC.options[:episode_duration]) if UHC.options[:episode_duration] > 0
+      end
+    end
+
+    class EpisodeTask
+      def initialize(time)
+        @time = time
+      end
+
+      def perform!
+        $mcl.server.traw "@a", "#{@time} minutes elapsed.", color: "light_purple"
+        $mcl.scheduler.schedule "episode marker", Time.now+UHC.options[:episode_duration].minutes, EpisodeTask.new(@time+UHC.options[:episode_duration])
+      end
+    end
+
+    class CommandTask
+      def initialize command
+        @command = command
+      end
+
+      def perform!
+        $mcl.server.invoke @command
+      end
+    end
+  end
+end
